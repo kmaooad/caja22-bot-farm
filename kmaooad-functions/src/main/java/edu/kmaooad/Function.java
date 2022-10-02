80af81dbc4a8e44570b3fd0c707374a34fc94bec
Ivan Pohodichev <ivan.pohodichev@gameloft.com>
2022-10-02 15:05:48 +0300

    Add proper msg response

--- kmaooad-functions/src/main/java/edu/kmaooad/Function.java
+++ kmaooad-functions/src/main/java/edu/kmaooad/Function.java
@@ -9,12 +9,17 @@ import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * Azure Functions with HTTP Trigger. 
+ * Azure Functions with HTTP Trigger.
  */
 public class Function {
+
+    private final Pattern pattern = Pattern.compile("(?:\"message_id\":)([0-9]+)");
     /**
      * This function listens at endpoint "/api/TelegramWebhook". To invoke it using "curl" command in bash:
      * curl -d "HTTP Body" {your host}/api/TelegramWebhook
@@ -22,19 +27,28 @@ public class Function {
     @FunctionName("TelegramWebhook")
     public HttpResponseMessage run(
             @HttpTrigger(
-                name = "req",
-                methods = {HttpMethod.POST},
-                authLevel = AuthorizationLevel.FUNCTION)
-                HttpRequestMessage<Optional<String>> request,
+                    name = "req",
+                    methods = {HttpMethod.POST},
+                    authLevel = AuthorizationLevel.FUNCTION)
+                    HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed a request.");
 
-        final String name = request.getBody().orElse(null);
+        final String message = request.getBody().orElse(null);
+        if (message == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                    .body("Please pass a name in the request body")
+                    .build();
+        }
 
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name in the request body").build();
+        final Matcher matcher = pattern.matcher(message);
+        if(!matcher.find()) {
+            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Can't get message_id from request")
+                    .build();
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+            final Integer messageId = Integer.parseInt(matcher.group(1));
+            return request.createResponseBuilder(HttpStatus.OK)
+                    .body(messageId).build();
         }
     }
 }
